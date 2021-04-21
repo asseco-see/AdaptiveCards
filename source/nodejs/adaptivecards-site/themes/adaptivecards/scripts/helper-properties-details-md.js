e3932f1d7812770a830cebfd732a9bf7e76c6f18
Nikola Stojanovic <nikola.stojanovic@asseco-see.rs>
2021-04-21 11:19:18 +0200

    Genearte markdown files

---
+++ source/nodejs/adaptivecards-site/themes/adaptivecards/scripts/helper-properties-details-md.js
@@ -0,0 +1,21 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+"use strict";
+
+var typedschema = require("ac-typed-schema");
+var marked = require("marked");
+var fs = require("fs");
+var path = require("path");
+
+hexo.extend.helper.register('properties_details_md', function (properties, elementVersion, isSpec) {
+	const codeAndCard = hexo.extend.helper.get('code_and_card').bind(hexo);
+
+	var md = "";
+
+	properties.forEach((property, name) => {
+		md += typedschema.markdown.createPropertyDetails(property, 3, null, false, true, elementVersion);
+		md += "\n";
+	});
+
+	return md;
+});
